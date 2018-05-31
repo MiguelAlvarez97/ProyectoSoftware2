78d8a57d0b82a3ac8211a7e97e705bfa188b43ce
ALEXIS FERNANDO MALDONADO TAPIA <alexis.maldonado@epn.edu.ec>
2018-05-31 00:34:32 -0500

    Modificacion GUI Index

--- index.php
+++ index.php
@@ -1,32 +1,31 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="es">
-    <head>
+<head>
 
-        <meta charset="utf-8"></meta>
-        <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css"></link>
-        <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap-responsive.css"></link>
-        <link rel="stylesheet" type="text/css" href="estilos/estilos.css"></link>
-        <title>Proyecto SGOA</title>
-    </head>
-    <body>
-        <form action="aplicacion/validar.php" method="post">
-            <h2 style="color: #0000FF; ">Inicio de sesión</h2>
-            <select class= "form-control" name="tipo_usuario" dir="ltr" required>
-                <option value="">Seleccione tipo de usuario</option>
-                <option value="ADM">Administrador</option>
-                <option value="PRO">Profesor</option>
-                <option value="EST">Estudiante</option>
-            </select></br>
-            <input class="form-control" type="text" placeholder="&#128104 Usuario" required name="user"></input></br>
-            <label></label>
-            <input class="form-control" type="password" placeholder="&#128273; Contraseña" required name="pass"></input>
-            <label>&nbsp;</label></br>
-            <input class="btn btn-primary" type="submit"  value="Aceptar"></br>
-                <label></label>
-                <label></label>
-                <td width="50%"> <a href="aplicacion/formularios_registro/RegistrarProfesor.php"> Registrar Profesor</a></td></br>
-                <label></label>
-                <td width="50%" align="right" valign="middle"><a href="aplicacion/formularios_registro/RegistrarEstudiante.php"> Registrar Estudiante</a></td>
-        </form>
-    </body>
+    <meta charset="utf-8"></meta>
+    <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css"></link>
+    <link rel="stylesheet" type="text/css" href="estilos/style.css"></link>
+    <title>Proyecto SGOA</title>
+</head>
+<body>
+<form action="aplicacion/validar.php" method="post">
+    <h2 style="color: #0000FF; font-size: 250%;">Repositorio Objetos de Aprendizaje</h2>
+    <select class= "form-control" name="tipo_usuario" dir="ltr" required>
+        <option value="">Tipo de Usuario</option>
+        <option value="ADM">Administrador</option>
+        <option value="PRO">Docente</option>
+        <option value="EST">Estudiante</option>
+    </select></br>
+    <input class="form-control" type="text" placeholder="&#128102; Nombre de Usuario." required name="user"></input></br>
+    <input class="form-control" type="password" placeholder="&#128274; Contraseña" required name="pass"></input>
+    <h2 style = "color: #0000FF; font-size: 80%"; align="right"> ¿Olvidó su contraseña? </h2>
+    <input class="btn btn-primary" type="submit"  value="Log-In">
+</form>
+<form method="post">
+    <h2 style="color: #0000FF; font-size: 250%;">Registrate</h2>
+    <td width="50%"> <a href="aplicacion/formularios_registro/RegistrarProfesor.php"> Registrar Profesor</a></td></br>
+    <label></label>
+    <td width="50%" align="right" valign="middle"><a href="aplicacion/formularios_registro/RegistrarEstudiante.php"> Registrar Estudiante</a></td>
+</form>
+</body>
 </html>
