385263eb9cae2475c0121ff843b533fea2fb3e24
jossued <jossuedutan@gmail.com>
2018-06-17 22:57:40 -0500

    Corregidos navbar, footer. Funcionando login, conocer más, ayuda, previsualizar.

--- sgoa/aplicacion/modulos_administrador/adm_ejecutar_comentar.php
+++ sgoa/aplicacion/modulos_administrador/adm_ejecutar_comentar.php
@@ -4,12 +4,19 @@ require '../clases_negocio/funciones_oa_profesor.php';
 
 $id_objeto_aprendizaje = filter_input(INPUT_POST, 'id_objeto_aprendizaje');
 $contenido = filter_input(INPUT_POST, 'contenido');
+
+$carpeta = "imagenes/";
+opendir($carpeta);
+$destino = $carpeta.$_FILES['file']['name'];
+copy($_FILES['file']['tmp_name'], $destino);
+$nombre = $_FILES['file']['name'];
+
 if (insertar_comentario($contenido, $_SESSION['id'], $id_objeto_aprendizaje)) {
 
     echo '<script charset="UTF-8">alert("Su comentario se inserto correctamente")</script> ';
-    echo "<script>location.href='adm_buscar.php'</script>";
+    echo '<script>location.href = "adm_comentarios.php?id=' . $id_objeto_aprendizaje . '"</script>';
 } else {
     echo '<script charset="UTF-8">alert("No se pudo insertar su comentario. ")</script> ';
-    echo "<script>location.href='adm_buscar.php'</script>";
+    echo '<script>"adm_comentarios.php?id=' . $id_objeto_aprendizaje . '"</script>';
 }
 ?>
